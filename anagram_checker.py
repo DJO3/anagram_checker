d625f5c1fcdeaabf34c511b57f25eb0431c8691a
Dave O'Connor <DJO3@users.noreply.github.com>
2015-05-13 18:36:56 -0400

    implemented new anagram checker

--- anagram_checker.py
+++ anagram_checker.py
@@ -1,42 +1,15 @@
-## Checks if two words are anagrams
+#Given a list of words L, find all the anagrams in L in the order in which they appear in L.
+L = ["pool", "loco", "cool", "stain", "satin", "pretty", "nice", "loop"]
 
-def ana(search, target):
-    L1 = []
-    L2 = []
-    if target == search:
-        return 0
-    for i in target:
-        L1.append(i)
-    for j in search:
-        L2.append(j)
-    L1.sort(), L2.sort()
-    if L1 == L2:
-        return 1
-    else:
-        return 0
+def anagram_checker(anagrams):
+    anagrams_sorted = ["".join(sorted(x)) for x in anagrams]
+    words = []
+    for word in anagrams:
+        sorted_word = "".join(sorted(word))
+        if anagrams_sorted.count(sorted_word) > 1 and word not in words:
+            words.append(word)
+    return words
 
-## For word in list, identifies number of anagrams of word within same list.
-## Outputs as a dictionary.
-
-def main(words):
-    result = {}
-    for word in words:
-        num = 0
-        pos = 0
-        while pos+1 <= len(words):
-            num = num + ana(words[pos],word)
-            pos = pos + 1
-            result[word] = num
-    return result
-
-list1 = ['bat','dog','god','bat','cat','tab','ogd','good','dogg','tab']
-
-print main(list1)
-
-##search,target = 'dog', 'god'
-##search2,target2 = 'cat', 'dog'
-##search3,target3 = 'good', 'dogg'
-##
-##print ana(search, target) 
-##print ana(search2, target2)
-##print ana(search3,target3)
+if __name__ == '__main__':
+    print(anagram_checker(L))
+    
